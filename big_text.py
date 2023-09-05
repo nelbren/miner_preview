b2da216dc5e925119292af54d6b0ba65389dd90b
Nelbren <nelbren@gmail.com>
2023-09-04 22:51:42 -0600

    Some fixes 050

--- big_text.py
+++ big_text.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ big_text.py - show custom big numbers
-    v0.1.3 - 2023-06-18 - nelbren@nelbren.com"""
+    v0.1.4 - 2023-08-27 - nelbren@nelbren.com"""
 import sys
 from random import randint
 from rich.console import Console
@@ -47,6 +47,18 @@ no2 = [
     [1, 1, 1, 1, 1, 1, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
 ]
+
+no2 = [
+    [2, 1, 1, 1, 1, 1, 2, 0],
+    [1, 3, 0, 0, 0, 3, 1, 0],
+    [0, 0, 0, 0, 0, 2, 1, 0],
+    [2, 1, 1, 1, 1, 1, 3, 0],
+    [1, 3, 0, 0, 0, 0, 0, 0],
+    [1, 2, 0, 0, 0, 0, 2, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 no3 = [
     [2, 1, 1, 1, 1, 1, 2, 0],
     [1, 3, 0, 0, 0, 3, 1, 0],
